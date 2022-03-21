4e2e8ba1042612bece4799f8f5375af14da130ac
Samson Achiaga <sirhumble84@gmail.com>
2022-03-21 16:18:40 +0100

    Initial commit

---
+++ src/main/kotlin/com/certified/springauth/SpringauthApplication.kt
@@ -0,0 +1,11 @@
+package com.certified.springauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringauthApplication>(*args)
+}
---
+++ src/test/kotlin/com/certified/springauth/SpringauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.certified.springauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringauthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
